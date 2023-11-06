561f1a9b26ec73c9eeabf6c982f99e92a07d4a23
Indrajith <mailtoindrajith>
2023-11-06 19:41:29 +0530

    First maven project

---
+++ src/main/java/supertech/com/App.java
@@ -0,0 +1,13 @@
+package supertech.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
